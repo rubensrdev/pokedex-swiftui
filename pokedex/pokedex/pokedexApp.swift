0f9c10c4b14a157937967dfe37b8d3fb767d5c35
Rubén <ruben.sr.apple@gmail.com>
2024-03-28 12:34:00 +0100

    Creado proyecto en xcode

---
+++ pokedex/pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Ruben on 28/3/24.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
